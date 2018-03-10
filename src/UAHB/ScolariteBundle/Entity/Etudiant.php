b5d30f3af5aab0a90783528b93a29021ecae2038
Baye Niass <bniass@gmail.com>
2018-03-10 09:09:18 +0000

    upadate langue model1

--- src/UAHB/ScolariteBundle/Entity/Etudiant.php
+++ src/UAHB/ScolariteBundle/Entity/Etudiant.php
@@ -181,10 +181,19 @@ class Etudiant
     private $commentConnaisTuUahb;
 
 
+   
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->telephones = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
     /**
      * Get id
      *
-     * @return int
+     * @return integer
      */
     public function getId()
     {
@@ -336,78 +345,6 @@ class Etudiant
     }
 
     /**
-     * Set telFixe
-     *
-     * @param string $telFixe
-     *
-     * @return Etudiant
-     */
-    public function setTelFixe($telFixe)
-    {
-        $this->telFixe = $telFixe;
-
-        return $this;
-    }
-
-    /**
-     * Get telFixe
-     *
-     * @return string
-     */
-    public function getTelFixe()
-    {
-        return $this->telFixe;
-    }
-
-    /**
-     * Set telPortable1
-     *
-     * @param string $telPortable1
-     *
-     * @return Etudiant
-     */
-    public function setTelPortable1($telPortable1)
-    {
-        $this->telPortable1 = $telPortable1;
-
-        return $this;
-    }
-
-    /**
-     * Get telPortable1
-     *
-     * @return string
-     */
-    public function getTelPortable1()
-    {
-        return $this->telPortable1;
-    }
-
-    /**
-     * Set telPostable2
-     *
-     * @param string $telPostable2
-     *
-     * @return Etudiant
-     */
-    public function setTelPostable2($telPostable2)
-    {
-        $this->telPostable2 = $telPostable2;
-
-        return $this;
-    }
-
-    /**
-     * Get telPostable2
-     *
-     * @return string
-     */
-    public function getTelPostable2()
-    {
-        return $this->telPostable2;
-    }
-
-    /**
      * Set adresse
      *
      * @param string $adresse
@@ -456,555 +393,27 @@ class Etudiant
     }
 
     /**
-     * Set nomPere
+     * Set nbenfant
      *
-     * @param string $nomPere
+     * @param integer $nbenfant
      *
      * @return Etudiant
      */
-    public function setNomPere($nomPere)
+    public function setNbenfant($nbenfant)
     {
-        $this->nomPere = $nomPere;
+        $this->nbenfant = $nbenfant;
 
         return $this;
     }
 
     /**
-     * Get nomPere
+     * Get nbenfant
      *
-     * @return string
+     * @return integer
      */
-    public function getNomPere()
+    public function getNbenfant()
     {
-        return $this->nomPere;
-    }
-
-    /**
-     * Set professionPere
-     *
-     * @param string $professionPere
-     *
-     * @return Etudiant
-     */
-    public function setProfessionPere($professionPere)
-    {
-        $this->professionPere = $professionPere;
-
-        return $this;
-    }
-
-    /**
-     * Get professionPere
-     *
-     * @return string
-     */
-    public function getProfessionPere()
-    {
-        return $this->professionPere;
-    }
-
-    /**
-     * Set adressePere
-     *
-     * @param string $adressePere
-     *
-     * @return Etudiant
-     */
-    public function setAdressePere($adressePere)
-    {
-        $this->adressePere = $adressePere;
-
-        return $this;
-    }
-
-    /**
-     * Get adressePere
-     *
-     * @return string
-     */
-    public function getAdressePere()
-    {
-        return $this->adressePere;
-    }
-
-    /**
-     * Set telPere
-     *
-     * @param string $telPere
-     *
-     * @return Etudiant
-     */
-    public function setTelPere($telPere)
-    {
-        $this->telPere = $telPere;
-
-        return $this;
-    }
-
-    /**
-     * Get telPere
-     *
-     * @return string
-     */
-    public function getTelPere()
-    {
-        return $this->telPere;
-    }
-
-    /**
-     * Set nomMere
-     *
-     * @param string $nomMere
-     *
-     * @return Etudiant
-     */
-    public function setNomMere($nomMere)
-    {
-        $this->nomMere = $nomMere;
-
-        return $this;
-    }
-
-    /**
-     * Get nomMere
-     *
-     * @return string
-     */
-    public function getNomMere()
-    {
-        return $this->nomMere;
-    }
-
-    /**
-     * Set prenomMere
-     *
-     * @param string $prenomMere
-     *
-     * @return Etudiant
-     */
-    public function setPrenomMere($prenomMere)
-    {
-        $this->prenomMere = $prenomMere;
-
-        return $this;
-    }
-
-    /**
-     * Get prenomMere
-     *
-     * @return string
-     */
-    public function getPrenomMere()
-    {
-        return $this->prenomMere;
-    }
-
-    /**
-     * Set professionMere
-     *
-     * @param string $professionMere
-     *
-     * @return Etudiant
-     */
-    public function setProfessionMere($professionMere)
-    {
-        $this->professionMere = $professionMere;
-
-        return $this;
-    }
-
-    /**
-     * Get professionMere
-     *
-     * @return string
-     */
-    public function getProfessionMere()
-    {
-        return $this->professionMere;
-    }
-
-    /**
-     * Set adresseMere
-     *
-     * @param string $adresseMere
-     *
-     * @return Etudiant
-     */
-    public function setAdresseMere($adresseMere)
-    {
-        $this->adresseMere = $adresseMere;
-
-        return $this;
-    }
-
-    /**
-     * Get adresseMere
-     *
-     * @return string
-     */
-    public function getAdresseMere()
-    {
-        return $this->adresseMere;
-    }
-
-    /**
-     * Set telMere
-     *
-     * @param string $telMere
-     *
-     * @return Etudiant
-     */
-    public function setTelMere($telMere)
-    {
-        $this->telMere = $telMere;
-
-        return $this;
-    }
-
-    /**
-     * Get telMere
-     *
-     * @return string
-     */
-    public function getTelMere()
-    {
-        return $this->telMere;
-    }
-
-    /**
-     * Set langue1
-     *
-     * @param string $langue1
-     *
-     * @return Etudiant
-     */
-    public function setLangue1($langue1)
-    {
-        $this->langue1 = $langue1;
-
-        return $this;
-    }
-
-    /**
-     * Get langue1
-     *
-     * @return string
-     */
-    public function getLangue1()
-    {
-        return $this->langue1;
-    }
-
-    /**
-     * Set niveauLangue1
-     *
-     * @param string $niveauLangue1
-     *
-     * @return Etudiant
-     */
-    public function setNiveauLangue1($niveauLangue1)
-    {
-        $this->niveauLangue1 = $niveauLangue1;
-
-        return $this;
-    }
-
-    /**
-     * Get niveauLangue1
-     *
-     * @return string
-     */
-    public function getNiveauLangue1()
-    {
-        return $this->niveauLangue1;
-    }
-
-    /**
-     * Set langue2
-     *
-     * @param string $langue2
-     *
-     * @return Etudiant
-     */
-    public function setLangue2($langue2)
-    {
-        $this->langue2 = $langue2;
-
-        return $this;
-    }
-
-    /**
-     * Get langue2
-     *
-     * @return string
-     */
-    public function getLangue2()
-    {
-        return $this->langue2;
-    }
-
-    /**
-     * Set niveauLangue2
-     *
-     * @param string $niveauLangue2
-     *
-     * @return Etudiant
-     */
-    public function setNiveauLangue2($niveauLangue2)
-    {
-        $this->niveauLangue2 = $niveauLangue2;
-
-        return $this;
-    }
-
-    /**
-     * Get niveauLangue2
-     *
-     * @return string
-     */
-    public function getNiveauLangue2()
-    {
-        return $this->niveauLangue2;
-    }
-
-    /**
-     * Set langue3
-     *
-     * @param string $langue3
-     *
-     * @return Etudiant
-     */
-    public function setLangue3($langue3)
-    {
-        $this->langue3 = $langue3;
-
-        return $this;
-    }
-
-    /**
-     * Get langue3
-     *
-     * @return string
-     */
-    public function getLangue3()
-    {
-        return $this->langue3;
-    }
-
-    /**
-     * Set niveauLangue3
-     *
-     * @param string $niveauLangue3
-     *
-     * @return Etudiant
-     */
-    public function setNiveauLangue3($niveauLangue3)
-    {
-        $this->niveauLangue3 = $niveauLangue3;
-
-        return $this;
-    }
-
-    /**
-     * Get niveauLangue3
-     *
-     * @return string
-     */
-    public function getNiveauLangue3()
-    {
-        return $this->niveauLangue3;
-    }
-
-    /**
-     * Set nomTuteur
-     *
-     * @param string $nomTuteur
-     *
-     * @return Etudiant
-     */
-    public function setNomTuteur($nomTuteur)
-    {
-        $this->nomTuteur = $nomTuteur;
-
-        return $this;
-    }
-
-    /**
-     * Get nomTuteur
-     *
-     * @return string
-     */
-    public function getNomTuteur()
-    {
-        return $this->nomTuteur;
-    }
-
-    /**
-     * Set prenomTuteur
-     *
-     * @param string $prenomTuteur
-     *
-     * @return Etudiant
-     */
-    public function setPrenomTuteur($prenomTuteur)
-    {
-        $this->prenomTuteur = $prenomTuteur;
-
-        return $this;
-    }
-
-    /**
-     * Get prenomTuteur
-     *
-     * @return string
-     */
-    public function getPrenomTuteur()
-    {
-        return $this->prenomTuteur;
-    }
-
-    /**
-     * Set professionTuteur
-     *
-     * @param string $professionTuteur
-     *
-     * @return Etudiant
-     */
-    public function setProfessionTuteur($professionTuteur)
-    {
-        $this->professionTuteur = $professionTuteur;
-
-        return $this;
-    }
-
-    /**
-     * Get professionTuteur
-     *
-     * @return string
-     */
-    public function getProfessionTuteur()
-    {
-        return $this->professionTuteur;
-    }
-
-    /**
-     * Set adresseTuteur
-     *
-     * @param string $adresseTuteur
-     *
-     * @return Etudiant
-     */
-    public function setAdresseTuteur($adresseTuteur)
-    {
-        $this->adresseTuteur = $adresseTuteur;
-
-        return $this;
-    }
-
-    /**
-     * Get adresseTuteur
-     *
-     * @return string
-     */
-    public function getAdresseTuteur()
-    {
-        return $this->adresseTuteur;
-    }
-
-    /**
-     * Set telTuteur
-     *
-     * @param string $telTuteur
-     *
-     * @return Etudiant
-     */
-    public function setTelTuteur($telTuteur)
-    {
-        $this->telTuteur = $telTuteur;
-
-        return $this;
-    }
-
-    /**
-     * Get telTuteur
-     *
-     * @return string
-     */
-    public function getTelTuteur()
-    {
-        return $this->telTuteur;
-    }
-
-    /**
-     * Set paysOrigine
-     *
-     * @param \UAHB\ScolariteBundle\Entity\Pays $paysOrigine
-     *
-     * @return Etudiant
-     */
-    public function setPaysOrigine(\UAHB\ScolariteBundle\Entity\Pays $paysOrigine)
-    {
-        $this->paysOrigine = $paysOrigine;
-
-        return $this;
-    }
-
-    /**
-     * Get paysOrigine
-     *
-     * @return \UAHB\ScolariteBundle\Entity\Pays
-     */
-    public function getPaysOrigine()
-    {
-        return $this->paysOrigine;
-    }
-
-    /**
-     * Set nationalite
-     *
-     * @param \UAHB\ScolariteBundle\Entity\Pays $nationalite
-     *
-     * @return Etudiant
-     */
-    public function setNationalite(\UAHB\ScolariteBundle\Entity\Pays $nationalite)
-    {
-        $this->nationalite = $nationalite;
-
-        return $this;
-    }
-
-    /**
-     * Get nationalite
-     *
-     * @return \UAHB\ScolariteBundle\Entity\Pays
-     */
-    public function getNationalite()
-    {
-        return $this->nationalite;
-    }
-
-    /**
-     * Set diplome
-     *
-     * @param \UAHB\ScolariteBundle\Entity\Diplome $diplome
-     *
-     * @return Etudiant
-     */
-    public function setDiplome(\UAHB\ScolariteBundle\Entity\Diplome $diplome = null)
-    {
-        $this->diplome = $diplome;
-
-        return $this;
-    }
-
-    /**
-     * Get diplome
-     *
-     * @return \UAHB\ScolariteBundle\Entity\Diplome
-     */
-    public function getDiplome()
-    {
-        return $this->diplome;
+        return $this->nbenfant;
     }
 
     /**
@@ -1152,277 +561,6 @@ class Etudiant
     }
 
     /**
-     * Set sexeTuteur
-     *
-     * @param string $sexeTuteur
-     *
-     * @return Etudiant
-     */
-    public function setSexeTuteur($sexeTuteur)
-    {
-        $this->sexeTuteur = $sexeTuteur;
-
-        return $this;
-    }
-
-    /**
-     * Get sexeTuteur
-     *
-     * @return string
-     */
-    public function getSexeTuteur()
-    {
-        return $this->sexeTuteur;
-    }
-
-    /**
-     * Set emailTuteur
-     *
-     * @param string $emailTuteur
-     *
-     * @return Etudiant
-     */
-    public function setEmailTuteur($emailTuteur)
-    {
-        $this->emailTuteur = $emailTuteur;
-
-        return $this;
-    }
-
-    /**
-     * Get emailTuteur
-     *
-     * @return string
-     */
-    public function getEmailTuteur()
-    {
-        return $this->emailTuteur;
-    }
-
-    /**
-     * Set nbenfant
-     *
-     * @param integer $nbenfant
-     *
-     * @return Etudiant
-     */
-    public function setNbenfant($nbenfant)
-    {
-        $this->nbenfant = $nbenfant;
-
-        return $this;
-    }
-
-    /**
-     * Get nbenfant
-     *
-     * @return integer
-     */
-    public function getNbenfant()
-    {
-        return $this->nbenfant;
-    }
-
-    /**
-     * Set image
-     *
-     * @param \UAHB\ScolariteBundle\Entity\Image $image
-     *
-     * @return Etudiant
-     */
-    public function setImage(\UAHB\ScolariteBundle\Entity\Image $image = null)
-    {
-        $this->image = $image;
-
-        return $this;
-    }
-
-    /**
-     * Get image
-     *
-     * @return \UAHB\ScolariteBundle\Entity\Image
-     */
-    public function getImage()
-    {
-        return $this->image;
-    }
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->telephones = new \Doctrine\Common\Collections\ArrayCollection();
-    }
-
-    /**
-     * Set nomAcontacter
-     *
-     * @param string $nomAcontacter
-     *
-     * @return Etudiant
-     */
-    public function setNomAcontacter($nomAcontacter)
-    {
-        $this->nomAcontacter = $nomAcontacter;
-
-        return $this;
-    }
-
-    /**
-     * Get nomAcontacter
-     *
-     * @return string
-     */
-    public function getNomAcontacter()
-    {
-        return $this->nomAcontacter;
-    }
-
-    /**
-     * Set prenomAcontacter
-     *
-     * @param string $prenomAcontacter
-     *
-     * @return Etudiant
-     */
-    public function setPrenomAcontacter($prenomAcontacter)
-    {
-        $this->prenomAcontacter = $prenomAcontacter;
-
-        return $this;
-    }
-
-    /**
-     * Get prenomAcontacter
-     *
-     * @return string
-     */
-    public function getPrenomAcontacter()
-    {
-        return $this->prenomAcontacter;
-    }
-
-    /**
-     * Set professionAcontacter
-     *
-     * @param string $professionAcontacter
-     *
-     * @return Etudiant
-     */
-    public function setProfessionAcontacter($professionAcontacter)
-    {
-        $this->professionAcontacter = $professionAcontacter;
-
-        return $this;
-    }
-
-    /**
-     * Get professionAcontacter
-     *
-     * @return string
-     */
-    public function getProfessionAcontacter()
-    {
-        return $this->professionAcontacter;
-    }
-
-    /**
-     * Set adresseAcontacter
-     *
-     * @param string $adresseAcontacter
-     *
-     * @return Etudiant
-     */
-    public function setAdresseAcontacter($adresseAcontacter)
-    {
-        $this->adresseAcontacter = $adresseAcontacter;
-
-        return $this;
-    }
-
-    /**
-     * Get adresseAcontacter
-     *
-     * @return string
-     */
-    public function getAdresseAcontacter()
-    {
-        return $this->adresseAcontacter;
-    }
-
-    /**
-     * Set telAcontacter
-     *
-     * @param string $telAcontacter
-     *
-     * @return Etudiant
-     */
-    public function setTelAcontacter($telAcontacter)
-    {
-        $this->telAcontacter = $telAcontacter;
-
-        return $this;
-    }
-
-    /**
-     * Get telAcontacter
-     *
-     * @return string
-     */
-    public function getTelAcontacter()
-    {
-        return $this->telAcontacter;
-    }
-
-    /**
-     * Set sexeAcontacter
-     *
-     * @param string $sexeAcontacter
-     *
-     * @return Etudiant
-     */
-    public function setSexeAcontacter($sexeAcontacter)
-    {
-        $this->sexeAcontacter = $sexeAcontacter;
-
-        return $this;
-    }
-
-    /**
-     * Get sexeAcontacter
-     *
-     * @return string
-     */
-    public function getSexeAcontacter()
-    {
-        return $this->sexeAcontacter;
-    }
-
-    /**
-     * Set emailAcontacter
-     *
-     * @param string $emailAcontacter
-     *
-     * @return Etudiant
-     */
-    public function setEmailAcontacter($emailAcontacter)
-    {
-        $this->emailAcontacter = $emailAcontacter;
-
-        return $this;
-    }
-
-    /**
-     * Get emailAcontacter
-     *
-     * @return string
-     */
-    public function getEmailAcontacter()
-    {
-        return $this->emailAcontacter;
-    }
-
-    /**
      * Add telephone
      *
      * @param \UAHB\ScolariteBundle\Entity\Telephone $telephone
@@ -1463,7 +601,7 @@ class Etudiant
      *
      * @return Etudiant
      */
-    public function setPere(\UAHB\ScolariteBundle\Entity\Personne $pere = null)
+    public function setPere(\UAHB\ScolariteBundle\Entity\Personne $pere)
     {
         $this->pere = $pere;
 
@@ -1487,7 +625,7 @@ class Etudiant
      *
      * @return Etudiant
      */
-    public function setMere(\UAHB\ScolariteBundle\Entity\Personne $mere = null)
+    public function setMere(\UAHB\ScolariteBundle\Entity\Personne $mere)
     {
         $this->mere = $mere;
 
@@ -1550,5 +688,101 @@ class Etudiant
     public function getPersonneAcontacter()
     {
         return $this->personneAcontacter;
+    }
+
+    /**
+     * Set image
+     *
+     * @param \UAHB\ScolariteBundle\Entity\Image $image
+     *
+     * @return Etudiant
+     */
+    public function setImage(\UAHB\ScolariteBundle\Entity\Image $image = null)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return \UAHB\ScolariteBundle\Entity\Image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set paysOrigine
+     *
+     * @param \UAHB\ScolariteBundle\Entity\Pays $paysOrigine
+     *
+     * @return Etudiant
+     */
+    public function setPaysOrigine(\UAHB\ScolariteBundle\Entity\Pays $paysOrigine)
+    {
+        $this->paysOrigine = $paysOrigine;
+
+        return $this;
+    }
+
+    /**
+     * Get paysOrigine
+     *
+     * @return \UAHB\ScolariteBundle\Entity\Pays
+     */
+    public function getPaysOrigine()
+    {
+        return $this->paysOrigine;
+    }
+
+    /**
+     * Set nationalite
+     *
+     * @param \UAHB\ScolariteBundle\Entity\Pays $nationalite
+     *
+     * @return Etudiant
+     */
+    public function setNationalite(\UAHB\ScolariteBundle\Entity\Pays $nationalite)
+    {
+        $this->nationalite = $nationalite;
+
+        return $this;
+    }
+
+    /**
+     * Get nationalite
+     *
+     * @return \UAHB\ScolariteBundle\Entity\Pays
+     */
+    public function getNationalite()
+    {
+        return $this->nationalite;
+    }
+
+    /**
+     * Set diplome
+     *
+     * @param \UAHB\ScolariteBundle\Entity\Diplome $diplome
+     *
+     * @return Etudiant
+     */
+    public function setDiplome(\UAHB\ScolariteBundle\Entity\Diplome $diplome = null)
+    {
+        $this->diplome = $diplome;
+
+        return $this;
+    }
+
+    /**
+     * Get diplome
+     *
+     * @return \UAHB\ScolariteBundle\Entity\Diplome
+     */
+    public function getDiplome()
+    {
+        return $this->diplome;
     }
 }
